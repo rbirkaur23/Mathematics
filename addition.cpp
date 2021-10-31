7e4996f26b666f9236ece8cb47eea69a52e1b7c5
skaur01 <72157116+skaur01@users.noreply.github.com>
2021-10-31 19:41:13 +0530

    addition.cpp

---
+++ addition.cpp
@@ -0,0 +1,12 @@
+//  Calculate sum of natural numbers
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n,answer;
+    cout<<"Enter n ";
+    cin>>n;
+    answer=n*(n+1)/2;
+    cout<<"Sum "<<answer;
+    return 0;
+}
